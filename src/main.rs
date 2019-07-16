875d3603fc2634fd5165246b632892b50bbdc039
Diljit VJ <diljitvj@gmail.com>
2019-07-16 14:27:02 +0530

    Guess secret number : Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-use ferris_says::say; 
-use std::io::{stdout, BufWriter};
+// To read the user input, io library is needed. We need to bring the library to the scope
+use std::io;
 
 fn main() {
-    let stdout = stdout();
-    let out = b"Hello fellow Rustaceans!";
-    let width = 24;
+    println!("Guess the game.");
+    println!("Please input your guess : ");
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(out, width, &mut writer).unwrap();
+    let mut guess = String::new();
+    // mut creates a mutable variable
+    // String::new() creates a new instance of String
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    // Read the user input and save the value in the variable guess
+    // .expect handles the error if any
+
+    println!("You guessed {}", guess);
+    // {} is the placeholder for a variable
 }
